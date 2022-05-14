c8c84351e9995ae36ca1728eabd90228b27ac357
MediamNoctem <romAnastasia79@yandex.ru>
2022-05-14 13:40:07 +0300

    toFormDigitalSignature

--- Coursework/Form1.Designer.cs
+++ Coursework/Form1.Designer.cs
@@ -29,23 +29,57 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello!";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 170);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1116, 35);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(22, 266);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1116, 35);
+            this.textBox2.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1150, 726);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Century", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "Form1";
             this.Text = "Курсовая работа";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Coursework/Form1.cs
+++ Coursework/Form1.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Numerics;
 using System.IO;
 using System.Drawing.Imaging;
+using System.Text;
 
 namespace Coursework
 {
@@ -22,12 +23,11 @@ namespace Coursework
             Gost_34_11_2012 G = new Gost_34_11_2012(256);
             Gost_34_11_2012 G512 = new Gost_34_11_2012(512);
             byte[] message ={0x32,0x31};
-            byte[] res = G.GetHash(message);
-            byte[] res2 = G512.GetHash(message);
-            string h256 = BitConverter.ToString(res);
-            string h512 = BitConverter.ToString(res2);
-            MessageBox.Show(h256);
-            MessageBox.Show(h512);
+            DigitalSignature ds = new DigitalSignature();
+            BigInteger d = BigInteger.Parse("55441196065363246126355624130324183196576709222340016572108097750006097525544");
+            string signature = ds.toFormDigitalSignature(message, d);
+            
+            textBox1.Text = signature;
         }
     }
     public class DigitalSignature
@@ -35,7 +35,7 @@ namespace Coursework
         Gost_34_11_2012 G256 = new Gost_34_11_2012(256);
         BigInteger n = EllipticCurve.EllipticCurve.n;
         BigInteger e;
-
+        
         public string toFormDigitalSignature(byte[] message, BigInteger signatureKey)
         {
             byte[] hash = G256.GetHash(message);
@@ -54,6 +54,7 @@ namespace Coursework
             {
                 k = rnd.Next(0, n);
                 if (k == 0) continue;
+
                 C = G.scalMultNumByPointEC(k);
                 r = C.x % n;
                 if (r == 0) continue;
@@ -62,12 +63,42 @@ namespace Coursework
                 break;
             }
 
-            byte[] r_hash = G256.GetHash(r);
-            byte[] s_hash = G256.GetHash(s);
+            string digitalSignature, d;
+            byte[] r_hash = r.ToByteArray();
+            byte[] s_hash = s.ToByteArray();
+            Array.Reverse(r_hash);
+            Array.Reverse(s_hash);
+            r_hash = G256.GetHash(r_hash);
+            s_hash = G256.GetHash(s_hash);
 
-            string digitalSignature = (r_hash.ToString() + s_hash.ToString());
-            
+            digitalSignature = BitConverter.ToString(r_hash) + "-" + BitConverter.ToString(s_hash);
+
             return digitalSignature;
         }
     }
+    public static class RandomExtension
+    {
+        public static BigInteger Next(this Random random, BigInteger minValue, BigInteger maxValue)
+        {
+            int number_digits_min = minValue.ToString().Length, number_digits_max = maxValue.ToString().Length;
+            int number_digits_in_num = random.Next(number_digits_min, number_digits_max);
+            string num = "";
+            int digit;
+
+            for (int i = 0; i < number_digits_in_num; i++)
+            {
+                digit = random.Next(0, 10);
+                num += digit.ToString();
+            }
+            return BigInteger.Parse(num);
+        }
+    }
+
+    //public static class ConvertExtension
+    //{
+    //    public static string ToString(BigInteger value, int toBase)
+    //    {
+
+    //    }
+    //}
 }
