f73c22a62c8141a696d74604282bdacdf54c470b
MediamNoctem <romAnastasia79@yandex.ru>
2023-01-25 15:47:29 +0300

    update

--- DTA/lab1/src/com/company/Main.java
+++ DTA/lab1/src/com/company/Main.java
@@ -1,6 +1,9 @@
 package com.company;
 import mpi.*;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.io.*;
 
 
 public class Main {
@@ -116,15 +119,114 @@ public class Main {
         }
     }
 
+    public static ArrayList<int[]> readMatrixFromFile(String path) {
+        ArrayList<int[]> adjacencyMatrix = new ArrayList<>();
+        try {
+            try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+                String str;
+                while ((str = in.readLine()) != null) {
+                    int len = str.length() / 2 + 1;
+                    int[] c = new int[len];
+                    for (int i = 0; i < len; i++)
+                        c[i] = str.toCharArray()[i * 2] - 48;
+                    adjacencyMatrix.add(c);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error!");
+        }
+        return adjacencyMatrix;
+    }
+
+    public static ArrayList<int[]> complementMatrixWithZeros(ArrayList<int[]> m, int number_active_processors) {
+        int dimension_of_matrix = m.size();
+        int number_iterations_per_processor0 = (0 < dimension_of_matrix % number_active_processors) ? (dimension_of_matrix / number_active_processors + 1) : (dimension_of_matrix / number_active_processors);
+        int new_size = number_iterations_per_processor0 * number_active_processors;
+
+        ArrayList<int[]> new_m = new ArrayList<>();
+        for (int i = 0; i < new_size; i++) {
+            int[] tmp = new int[new_size];
+            if (i < dimension_of_matrix)
+                System.arraycopy(m.get(i), 0, tmp, 0, dimension_of_matrix);
+            new_m.add(tmp);
+        }
+        return new_m;
+    }
+
+    public static int maximumDegreeOfGraphVertex(ArrayList<int[]> adjacencyMatrix, int size, int rank) {
+        int dimension_of_matrix = adjacencyMatrix.size();
+        int number_active_processors = Math.min(size, dimension_of_matrix);
+        adjacencyMatrix = complementMatrixWithZeros(adjacencyMatrix, number_active_processors);
+        dimension_of_matrix = adjacencyMatrix.size();
+        int index = rank;
+        int[] tmp_maximum_degree = new int[] {0};
+        int[] maximum_degree = new int[] {0, 0};
+
+        if (rank < number_active_processors) {
+            while (index < dimension_of_matrix) {
+                for (int i = 0; i < dimension_of_matrix; i++) {
+                    tmp_maximum_degree[0] += adjacencyMatrix.get(index)[i] | adjacencyMatrix.get(i)[index];
+                }
+                MPI.COMM_WORLD.Reduce(tmp_maximum_degree, 0, maximum_degree, 0, 1, MPI.INT, MPI.MAX, 0);
+                index += number_active_processors;
+                maximum_degree[1] = Math.max(maximum_degree[0], maximum_degree[1]);
+                tmp_maximum_degree[0] = 0;
+            }
+        }
+        return maximum_degree[1];
+    }
+
+    public static boolean isGraphHypercubeParallel(int rank, int size, int n, int[] arr, int razm) {
+        MPI.COMM_WORLD.Bcast(arr, 0, n * n, MPI.INT, 0);
+
+        int[] temp_sum = new int[3];
+        for (int i = 0; i < n / size + (n % size > 0 ? 1 : 0); i++) {
+            temp_sum[1] = 0;
+            if (rank + size * i < n) {
+                for (int j = n * (rank + size * i) + rank + size * i; j < n * (rank + size * i + 1); j++) {
+                    temp_sum[0] += arr[j];
+                }
+                for (int j = n * (rank + size * i); j < n * (rank + size * i + 1); j++) {
+                    temp_sum[1] += arr[j];
+                }
+                if(temp_sum[1] != razm) temp_sum[2] += 1;
+            }
+        }
+        int[] sum = new int[2];
+        int[] t_sum = new int[2];
+        t_sum[0] = temp_sum[0];
+        t_sum[1] = temp_sum[2];
+
+        MPI.COMM_WORLD.Reduce(t_sum, 0, sum, 0, 2, MPI.INT, MPI.SUM, 0);
+        if (rank == 0) return sum[1] == 0 && ((n & (n - 1)) == 0) && sum[0] == (Math. pow(2,(razm-1)) * razm);
+        return false;
+    }
+
+    public static boolean isGraphHypercubeSerial(int n, int[] arr, int razm) {
+        int sum = 0;
+        for(int i = 0; i < n; i++)
+            for(int j = i; j<n; j++) {
+                sum += arr[i*n+j];
+            }
+        int sum2 = 0,sum3=0;
+        for(int i = 0; i < n; i++){
+            sum2=0;
+            for(int j = 0; j<n; j++)
+                sum2 += arr[i*n+j];
+            if(sum2!=razm) sum3++;
+        }
+        return sum3 == 0 && ((n & (n - 1)) == 0) && sum == (Math. pow(2,(razm-1)) * razm);
+    }
+
     public static void main(String[] args)
             throws Exception {
-        int[] s = { 0 };
+        /*int[] s = { 0 };
         int[] data = { 2016 };
         int[] buf = { 1, 3, 5 };
         int count, TAG = 0;
         Status st;
         int[] mas = { 14, 5, 9, 0, 80, 3, 1, 15 };
-
+        */
         MPI.Init(args);
         int rank = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
@@ -169,6 +271,55 @@ public class Main {
             for (int i = 0; i < count; i++)
                 System.out.print(buf[i] + " ");
             System.out.print("\n");
+        }*/
+
+//        #7-8
+//        №1
+        ArrayList<int[]> adjacencyMatrix = readMatrixFromFile("src/com/company/graph2.txt");
+        int m = maximumDegreeOfGraphVertex(adjacencyMatrix,size,rank);
+        if (rank == 0) {
+            for (int i = 0; i < adjacencyMatrix.size(); i++) {
+                System.out.println(Arrays.toString(adjacencyMatrix.get(i)));
+            }
+            System.out.println("Maximum degree of graph vertex = " + m);
+        }
+//        №2
+        /*ArrayList<int[]> adjacencyMatrix = readMatrixFromFile("src/com/company/graph4.txt");
+
+        int[] N = {1000, 2500, 4000};
+        for (int n : N) {
+            int[] arr = new int[n * n];
+            long time = 0;
+            if (rank == 0) {
+                System.out.println("n = " + n);
+
+            arr = new int[]{0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0};
+//            arr = new int[]{
+//                    0,1,0,1,0,0,1,0,
+//                    1,0,1,0,0,0,0,1,
+//                    0,1,0,1,1,0,0,0,
+//                    1,0,1,0,0,1,0,0,
+//                    0,0,1,0,0,1,0,1,
+//                    0,0,0,1,1,0,1,0,
+//                    1,0,0,0,0,1,0,1,
+//                    0,1,0,0,1,0,1,0
+//            };
+//                arr = gen_matr_gipercube(n,4);
+
+                time = System.currentTimeMillis();
+            }
+            for(int i = 1; i<5;i++) {
+                boolean res = false;
+//      res = lab8(rank, size, n, arr, i);
+                if (rank == 0) res = lab8_posled_alg(n,arr,i);
+                if(rank == 0)
+                    System.out.println("for dimension = " + i + " -> " + res);
+                if(res) break;
+            }
+            if (rank == 0) {
+                long t = System.currentTimeMillis();
+                System.out.println("Time = " + (t - time) + " ms");
+            }
         }*/
 
         MPI.Finalize();
